04a3138b51ddf1090c7540af3a7a3b794ca85d4d
Diaz Fernando <fernandodiaz@tdf.edu.ar>
2023-05-06 18:06:28 -0300

    get pistas

--- src/pista/pista.controller.ts
+++ src/pista/pista.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { PistaService } from './pista.service';
 
 @Controller('pista')
-export class PistaController {}
+export class PistaController {
+    
+    constructor(private pistaService: PistaService){}
+
+    @Get('pistas')
+    getPistas():any{
+        return this.pistaService.getPistas();
+    }
+
+    @Get('buscar/:id')
+    getPistaByID(@Param('id',ParseIntPipe) id:number):any{
+        return this.pistaService.getPistaByID(id);
+    
+    }
+}
--- src/pista/pista.service.ts
+++ src/pista/pista.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PistaService {}
+export class PistaService {
+     pistasAudio=[
+        {
+            "id":1,
+            "titulo": "titulo1",
+            "duracion": 123,
+            "interprete": "interprete1" 
+        },
+        {
+            "id":2,
+            "titulo": "titulo2",
+            "duracion": 55,
+            "interprete": "interprete2" 
+        },
+        {
+            "id":3,
+            "titulo": "titulo3",
+            "duracion": 12,
+            "interprete": "interprete3" 
+        },
+        {
+            "id":4,
+            "titulo": "titulo4",
+            "duracion": 140,
+            "interprete": "interprete1" 
+        }
+    ];
+
+    public getPistas():any{
+        return this.pistasAudio;
+    }
+
+    public getPistaByID(id:number):any{
+        let pista= this.pistasAudio.find(p => p.id === id);
+        return pista;
+    }
+
+}
