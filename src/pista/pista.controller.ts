78c89ea2f2b759c925a7c9ec8d0c0060ec9df122
Diaz Fernando <fernandodiaz@tdf.edu.ar>
2023-05-07 22:26:12 -0300

    se creo el post

--- src/pista/pista.controller.ts
+++ src/pista/pista.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { PistaService } from './pista.service';
 
 @Controller('pista')
@@ -6,14 +6,23 @@ export class PistaController {
     
     constructor(private pistaService: PistaService){}
 
+    //get para mostrar todas las pistas
     @Get('pistas')
     getPistas():any{
         return this.pistaService.getPistas();
     }
 
+    //get para buscar por ID
     @Get('buscar/:id')
     getPistaByID(@Param('id',ParseIntPipe) id:number):any{
         return this.pistaService.getPistaByID(id);
     
     }
+
+
+    @Post('crear')
+    crearPista(@Body() body:any):any{
+        return this.pistaService.crearPista(body);
+    }
+         
 }
--- src/pista/pista.service.ts
+++ src/pista/pista.service.ts
@@ -29,13 +29,29 @@ export class PistaService {
         }
     ];
 
+    // get para mostrar todas las pistas
     public getPistas():any{
         return this.pistasAudio;
     }
 
+    // get para buscar por ID
     public getPistaByID(id:number):any{
         let pista= this.pistasAudio.find(p => p.id === id);
         return pista;
+    }
+
+    //post para crear una pista
+    public crearPista(body:any):any{
+        let pista = {
+            "id":body.id,
+            "titulo": body.titulo,
+            "duracion": body.duracion,
+            "interprete": body.interprete
+        }
+        this.pistasAudio.push(pista);
+        return {"msj" : "Se creo una nueva pista",
+                "pista" : pista
+        }
     }
 
 }
