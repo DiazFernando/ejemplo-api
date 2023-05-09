bf04dd154cf41eeac4626cf8e5153080a57b5100
Diaz Fernando <fernandodiaz@tdf.edu.ar>
2023-05-09 19:58:39 -0300

    importo join

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PistaController } from './pista/pista.controller';
 import { PistaService } from './pista/pista.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 
 @Module({
-  imports: [],
+  imports: [ServeStaticModule.forRoot({ rootPath: join(__dirname,'..','client')})],
   controllers: [AppController, PistaController],
   providers: [AppService, PistaService],
 })
