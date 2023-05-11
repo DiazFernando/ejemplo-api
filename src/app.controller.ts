086acd8c53fd44d40b21fa93e3282251fc3b4dae
Diaz Fernando <fernandodiaz@tdf.edu.ar>
2023-05-10 21:07:19 -0300

    se creo una claculadora por url

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('api')
+@Controller('calculadora')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('sumar/:num1/:num2')
+  getSumar(@Param('num1', ParseIntPipe) num1:number,@Param('num2', ParseIntPipe) num2:number):number{
+    return this.appService.getSumar(num1, num2);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getSumar(num1:number,num2:number):number{
+    return (num1) + (num2);
+  }
 }
